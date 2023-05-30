324c179153d2326897f66aa10fdfe34baedefd59
LaputaMao <1344143983@qq.com>
2023-05-30 12:40:05 +0800

    p_finished

--- app.py
+++ app.py
@@ -779,7 +779,7 @@ def search():
 
     if filter_display_list_to_dict is not None:
         return jsonify(
-            {"code": 6200, "message": "推荐请求成功", "data": filter_display_list_to_dict,
+            {"code": 6200, "message": "搜索成功", "data": filter_display_list_to_dict,
              "pages": page_odject.pages})
     else:
         return {"code": 6501, "message": "请求页为空"}
@@ -832,3 +832,305 @@ def testsql():
 if __name__ == '__main__':
     db.create_all()
     app.run(host='0.0.0.0', debug=True)
+
+
+# -*- coding: UTF-8 -*-
+from math import log
+import operator
+import pickle
+from matplotlib.font_manager import FontProperties
+import matplotlib.pyplot as plt
+
+import matplotlib
+
+"""
+创建测试数据集
+"""
+
+
+def createDataSet():
+    dataSet = [[2, 0, 0, 0, 2, 0, 0, 0, 'no'],
+               [2, 0, 0, 1, 0, 0, 0, 1, 'no'],
+               [0, 1, 0, 1, 1, 2, 0, 0, 'yes'],
+               [0, 1, 1, 0, 1, 2, 0, 0, 'yes'],
+               [2, 0, 0, 0, 2, 2, 1, 1, 'no'],
+               [0, 0, 0, 0, 1, 0, 1, 0, 'no'],
+               [1, 0, 0, 1, 1, 0, 1, 1, 'no'],
+               [0, 1, 1, 0, 1, 1, 0, 0, 'yes'],
+               [2, 0, 0, 0, 2, 2, 1, 1, 'no'],
+               [1, 0, 1, 0, 1, 2, 0, 0, 'yes'],
+               [2, 0, 0, 1, 2, 0, 1, 1, 'no'],
+               [1, 1, 1, 1, 1, 2, 1, 0, 'yes'],
+               [1, 0, 1, 2, 1, 0, 0, 1, 'yes'],
+               [1, 0, 1, 2, 1, 2, 0, 0, 'yes'],
+               [2, 0, 1, 2, 1, 2, 0, 0, 'yes'],
+               [2, 0, 1, 1, 1, 1, 0, 0, 'no'],
+               [1, 1, 1, 1, 1, 2, 1, 0, 'no'],
+               [1, 0, 1, 2, 1, 1, 0, 1, 'no'],
+               [1, 0, 1, 2, 1, 1, 0, 0, 'no'],
+               [2, 0, 1, 2, 1, 1, 0, 0, 'no'],
+               [2, 0, 0, 1, 1, 1, 0, 0, 'no'],
+               [2, 1, 0, 1, 2, 0, 1, 1, 'no'],
+               [2, 1, 0, 2, 2, 1, 1, 0, 'no'],
+               [0, 0, 0, 0, 2, 0, 1, 1, 'no']]
+    labels = ['age', 'height', 'edu', 'salary', 'marital', 'occupation', 'house', 'car']  # 特征标签
+    return dataSet, labels
+
+
+"""
+计算给定数据集的经验熵
+"""
+
+
+def calcShannonEnt(dataSet):
+    numEntires = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]  # 提取标签(Label)信息
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0  # 经验熵
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntires
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt  # 返回经验熵
+
+
+"""
+按照给定特征划分数据集
+"""
+
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:  # 遍历数据集
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis + 1:])  # 将符合条件的添加到返回的数据集
+            retDataSet.append(reducedFeatVec)
+    return retDataSet  # 返回划分后的数据集
+
+
+"""
+选择最优特征
+"""
+
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0  # 信息增益
+    bestFeature = -1  # 最优特征的索引值
+    for i in range(numFeatures):  # 遍历所有特征
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0  # 经验条件熵
+
+        for value in uniqueVals:  # 计算信息增益
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        print("第%d个特征的增益为%.3f - %.3f =%.3f" % (i + 1, baseEntropy, newEntropy, infoGain))
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+
+"""
+统计classList中出现此处最多的元素(类标签)
+"""
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:  # 统计classList中每个元素出现的次数
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]  # 返回classList中出现次数最多的元素
+
+
+"""
+创建决策树
+"""
+
+
+def createTree(dataSet, labels, featLabels):
+    classList = [example[-1] for example in dataSet]  # 取分类标签
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1 or len(labels) == 0:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]  # 最优特征的标签
+    featLabels.append(bestFeatLabel)
+    myTree = {bestFeatLabel: {}}  # 根据最优特征的标签生成树
+    del (labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]  # 得到训练集中所有最优特征的属性值
+    uniqueVals = set(featValues)  # 去掉重复的属性值
+    for value in uniqueVals:  # 遍历特征，创建决策树。
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels, featLabels)
+
+    return myTree
+
+
+"""
+获取决策树叶子结点的数目
+"""
+
+
+def getNumLeafs(myTree):
+    numLeafs = 0  # 初始化叶子
+    firstStr = next(iter(
+        myTree))
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+
+"""
+获取决策树的层数
+"""
+
+
+def getTreeDepth(myTree):
+    maxDepth = 0  # 初始化决策树深度
+    firstStr = next(iter(
+        myTree))
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth: maxDepth = thisDepth  # 更新层数
+    return maxDepth
+
+
+"""
+绘制结点
+"""
+
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    arrow_args = dict(arrowstyle="<-")
+    font = FontProperties(fname=r"c:\windows\fonts\simsunb.ttf", size=14)  # 设置中文字体失败转而用英文
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',  # 绘制结点
+                            xytext=centerPt, textcoords='axes fraction',
+                            va="center", ha="center", bbox=nodeType, arrowprops=arrow_args, fontproperties=font)
+
+
+"""
+标注有向边属性值
+"""
+
+
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]  # 计算标注位置
+    yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
+
+
+"""
+绘制决策树
+"""
+
+
+def plotTree(myTree, parentPt, nodeTxt):
+    decisionNode = dict(boxstyle="sawtooth", fc="0.8")  # 设置结点格式
+    leafNode = dict(boxstyle="round4", fc="0.8")  # 设置叶结点格式
+    numLeafs = getNumLeafs(myTree)  # 获取决策树叶结点数目，决定了树的宽度
+    depth = getTreeDepth(myTree)  # 获取决策树层数
+    firstStr = next(iter(myTree))
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW, plotTree.yOff)  # 中心位置
+    plotMidText(cntrPt, parentPt, nodeTxt)  # 标注有向边属性值
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)  # 绘制结点
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0 / plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':  # 测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
+            plotTree(secondDict[key], cntrPt, str(key))  # 不是叶结点，递归调用继续绘制
+        else:  # 如果是叶结点，绘制叶结点，并标注有向边属性值
+            plotTree.xOff = plotTree.xOff + 1.0 / plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0 / plotTree.totalD
+
+
+"""
+创建绘制面板
+"""
+
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')  # 创建fig
+    fig.clf()  # 清空fig
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)  # 去掉x、y轴
+    plotTree.totalW = float(getNumLeafs(inTree))  # 获取决策树叶结点数目
+    plotTree.totalD = float(getTreeDepth(inTree))  # 获取决策树层数
+    plotTree.xOff = -0.5 / plotTree.totalW;
+    plotTree.yOff = 1.0;  # x偏移
+    plotTree(inTree, (0.5, 1.0), '')  # 绘制决策树
+    plt.show()  # 显示绘制结果
+
+
+"""
+使用决策树分类
+"""
+
+
+def classify(inputTree, featLabels, testVec):
+    firstStr = next(iter(inputTree))  # 获取决策树结点
+    secondDict = inputTree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__ == 'dict':
+                classLabel = classify(secondDict[key], featLabels, testVec)
+            else:
+                classLabel = secondDict[key]
+    return classLabel
+
+
+"""
+存储决策树待完善部分
+"""
+
+# def storeTree(inputTree, filename):
+#
+#         pickle.dump(inputTree, fw)
+
+"""
+读取决策树
+"""
+#
+# def grabTree(filename):
+#
+#     return pickle.load(fr)
+
+if __name__ == '__main__':
+
+    print(matplotlib.__version__)
+    dataSet, labels = createDataSet()
+    summ = 20
+    featLabels = []
+    myTree = createTree(dataSet, labels, featLabels)
+    createPlot(myTree)
+    for i in dataSet:
+        testVec = [1, 0, 1, 1, 2, 1, 0, 1]  # 测试数据
+        result = classify(myTree, featLabels, i)
+        # print(testVec)
+        if result == 'yes':
+            print(i, '心动')
+        if result == 'no':
+            print(i, '不心动')
+
